fe78c6c2ce3d70179b073831499e3ce85e7bf364
Frank Acquaye <acquayefrank@users.noreply.github.com>
2026-03-25 11:45:48 +0000

    Fix crux command and update spectrum files path

--- src/run_crux_timing_to_measure_runs.sh
+++ src/run_crux_timing_to_measure_runs.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-
 spectrum_files_folder=$(realpath "../data/spectrum_files")
 
-mapfile -t spectra_files < <(find "$spectra_files_path" -type f -name "*.mzML" -printf '%p\n' | sort -u)
+mapfile -t spectra_files < <(find "$spectrum_files_folder" -type f -name "*.mzML" -printf '%p\n' | sort -u)
 
 results_folder=$(realpath "../results")
 LOG_DIR="${results_folder}/crux_runs_timing_memory_logs"
 mkdir -p "$LOG_DIR"
 
-crux=$(realpath "../bin/crux-lfq")
+crux=$(realpath "../bin/crux")
 assign_confidence_file=$(realpath "../data/assign-confidence.target.txt")
 
 for i in {1..3}; do
@@ -19,7 +18,7 @@ for i in {1..3}; do
         echo "Selecting $num_files files:"
         selected_files=("${shuffled_files[@]:0:$num_files}")
         LOG_FILE="$LOG_DIR/iteration_${i}_${num_files}.log"
-        crux="${crux} lfq ${assign_confidence_file} ${selected_files[@]} --overwrite T --spectrum-parser mstoolkit"
-        /usr/bin/time -v $crux > "${LOG_FILE}" 2>&1
+        crux_cmd="${crux} lfq ${assign_confidence_file} ${selected_files[@]} --overwrite T --spectrum-parser mstoolkit"
+        /usr/bin/time -v $crux_cmd > "${LOG_FILE}" 2>&1
     done
 done
